9ed143c29feeb611049a0fe320c4a698c1a3ab74
Archit-Rajput11 <architrajput107@gmail.com>
2026-02-26 10:20:55 +0530

    Update EvaluateExpression.c

--- EvaluateExpression.c
+++ EvaluateExpression.c
@@ -122,16 +122,62 @@ int evalPostfixExpression(char* exp){
     }
     return operand[top1--];
 }
+
+int evalPrefixExpression(char* exp){
+    for(int i=strlen(exp)-1;i>=0; i--){
+        if(isdigit(exp[i])){
+            int num = 0,p = 1;
+            while(isdigit(exp[i])){
+                num = num*10 +(exp[i]-'0')*p;
+                p = p * 10;
+                i++;
+            }
+            i--;
+            operand[++top1] = num;
+        }
+        else if(exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/' || exp[i] == '^'){
+            int x = operand[top1--];
+            int y = operand[top1--];
+            int z;
+            switch(exp[i]){
+                case '+':
+                    z= y + x;
+                    break;
+                case '-':
+                    z= y - x;
+                    break;
+                case '*':
+                    z= y * x;
+                    break;
+                case '/':
+                    z = y / x;
+                    break;
+                case '^':
+                    z = (int)round(pow(y,x));
+                    break; 
+            }
+            operand[top1++] = z;
+        }
+    }
+    return operand[top1--];
+}
 int main(){
     char expr[100];
-    printf("Enter Prefix Expression: ");
+    printf("Enter Infix Expression: ");
     gets(expr);
-    int result = evalInfixExpression(expr);
+    int result;
+    result = evalInfixExpression(expr);
     printf("Result of expression %s = %d\n",expr,result);
 
     printf("Enter Postfix Expression: ");
     gets(expr);
-    int result = evalPostfixExpression(expr);
+    result = evalPostfixExpression(expr);
     printf("Result of expression %s = %d\n",expr,result);
     return 0;
+
+    printf("Enter Prefix Expression: ");
+    gets(expr);
+    result = evalPrefixExpression(expr);
+    printf("Result of expression %s = %d\n",expr,result);
+
 }
