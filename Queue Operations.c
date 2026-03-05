d276485c93b2b2508304a1973fb680b34e3d35c8
Archit-Rajput11 <architrajput107@gmail.com>
2026-03-05 20:37:46 +0530

    Update Queue Operations.c

--- Queue Operations.c
+++ Queue Operations.c
@@ -1,60 +1,58 @@
 #include<stdio.h>
 #define MAX 100
-int queue[MAX],front = -1, rear = -1;
-
+int queue[MAX], front = -1, rear = -1;
 int isFull(){
     return rear == MAX-1;
 }
-
 int isEmpty(){
     return front == -1;
 }
-
 void Enqueue(int data){
     if(isFull()){
         printf("Queue Overflow!!!\n");
         return;
     }
     queue[++rear] = data;
+
     if(front == -1)
         front = rear;
-    printf("\n%d inserted in queue\n",data);
-}
 
+    printf("%d inserted in queue\n", data);
+}
 int dequeue(){
-    if(isFull()){
-        printf("\nQueue Underflow!!1\n");
+    if(isEmpty()){
+        printf("Queue Underflow!!!\n");
         return -1;
     }
     int val = queue[front++];
     if(front > rear)
-        front = rear =- 1;
+        front = rear = -1;
+
     return val;
-
 }
-
 int peek(){
-    if(isFull()){
-        printf("\nQueue is empty!!!\n");
+    if(isEmpty()){
+        printf("Queue is empty!!!\n");
         return -1;
     }
+
     return queue[front];
 }
-
 void display(){
-    if(isFull()){
-        printf("\nQueue is empty!!1\n");
-        return -1;
+    if(isEmpty()){
+        printf("Queue is empty!!!\n");
+        return;
     }
-    for(int i = front;i <= rear;i++){
-        printf("%d", queue[i]);
+    printf("Queue elements: ");
+    for(int i = front; i <= rear; i++){
+        printf("%d ", queue[i]);
     }
     printf("\n");
 }
 int main(){
     int choice, value;
     while(1){
-        printf("1. Enqueue\n");
+        printf("\n1. Enqueue\n");
         printf("2. Dequeue\n");
         printf("3. Peek\n");
         printf("4. Display\n");
@@ -63,16 +61,28 @@ int main(){
         scanf("%d",&choice);
         switch(choice){
             case 1:
+                printf("Enter value: ");
+                scanf("%d",&value);
+                Enqueue(value);
                 break;
             case 2:
+                value = dequeue();
+                if(value != -1)
+                    printf("Deleted element: %d\n", value);
                 break;
             case 3:
+                value = peek();
+                if(value != -1)
+                    printf("Front element: %d\n", value);
                 break;
             case 4:
+                display();
                 break;
             case 5:
-                break;
+                printf("Exiting...\n");
+                return 0;
             default:
+                printf("Invalid Choice\n");
         }
     }
 }
