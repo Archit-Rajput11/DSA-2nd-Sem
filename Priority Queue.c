8566c3904a5d5d832d99a1af2727ab77a608d559
Archit-Rajput11 <architrajput107@gmail.com>
2026-03-09 10:19:16 +0530

    Update Priority Queue.c

--- Priority Queue.c
+++ Priority Queue.c
@@ -65,11 +65,48 @@ void peek(){
     }
 }
 void display(){
-    
+    if(isEmpty()){
+        printf("Queue is Empty!!!\n");
+        return;
+    }
+    for(int i =0; i<= rear; i++){
+        printf("%d --> %d\n",pqueue[i].priority,pqueue[i].data);
+    }
 }
 int main(){
     int choice, value;
     while(1){
-
+        printf("\n1. Enqueue\n");
+        printf("2. Dequeue\n");
+        printf("3. Peek\n");
+        printf("4. Display\n");
+        printf("5. Exit\n");
+        printf("Enter Choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+                printf("Enter value: ");
+                scanf("%d",&value);
+                Enqueue(value);
+                break;
+            case 2:
+                value = dequeue();
+                if(value != -1)
+                    printf("Deleted element: %d\n", value);
+                break;
+            case 3:
+                value = peek();
+                if(value != -1)
+                    printf("Front element: %d\n", value);
+                break;
+            case 4:
+                display();
+                break;
+            case 5:
+                printf("Exiting...\n");
+                return 0;
+            default:
+                printf("Invalid Choice\n");
+        }
     }
 }
