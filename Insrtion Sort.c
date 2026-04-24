0a566859aba5487fb7fe9c34246a25b42e27d1d7
Archit-Rajput11 <architrajput107@gmail.com>
2026-04-24 12:57:21 +0530

    Create Insrtion Sort.c

---
+++ Insrtion Sort.c
@@ -0,0 +1,26 @@
+//insertion sort
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter array size: ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter Array Elements: ");
+    for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    }
+    for(int i=1;i<n;i++){
+    int curr = arr[i];
+    int prev = i-1;
+        while(prev >= 0 && arr[prev]>curr){
+        arr[prev+1] = arr[prev];
+        prev--;
+        }
+        arr[prev+1]=curr;
+    }
+    printf("After sorting array elements are: ");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
